e7b3b83b6a14a1535a54b64ed5197bb5f66320e6
Masaki Nakazato <masaki.nakazato@brainpad.co.jp>
2021-12-01 15:02:38 +0900

    1st commit

---
+++ toutf8.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+
+import os
+
+def main():
+
+  files = os.listdir('.\sjis')
+  for file in files:
+    print(file)
+    with open('.\sjis\\' + file, encoding='shift_jis') as fi, open('.\\utf8\\' + file, 'w',encoding='utf-8') as fo:
+      fo.write(fi.read())
+
+if __name__ == '__main__':
+  main()
