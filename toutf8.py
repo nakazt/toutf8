e2b90351567c6ec4d98b2ca90b464166674cacb4
Masaki Nakazato <masaki.nakazato@brainpad.co.jp>
2021-12-01 15:20:32 +0900

    パスの区切りを / にする

--- toutf8.py
+++ toutf8.py
@@ -4,10 +4,11 @@ import os
 
 def main():
 
-  files = os.listdir('.\sjis')
+  files = os.listdir('./sjis')
   for file in files:
     print(file)
-    with open('.\\sjis\\' + file, encoding='shift_jis') as fi, open('.\\utf8\\' + file, 'w',encoding='utf-8') as fo:
+    # with open('.\\sjis\\' + file, encoding='shift_jis') as fi, open('.\\utf8\\' + file, 'w',encoding='utf-8') as fo:
+    with open('./sjis/' + file, encoding='shift_jis') as fi, open('./utf8/' + file, 'w',encoding='utf-8') as fo:
       fo.write(fi.read())
 
 if __name__ == '__main__':
