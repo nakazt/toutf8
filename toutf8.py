4a953d8443c42d1da81725a64d08793fa84a6103
Masaki Nakazato <masaki.nakazato@brainpad.co.jp>
2021-12-01 15:16:14 +0900

    微修正

--- toutf8.py
+++ toutf8.py
@@ -7,7 +7,7 @@ def main():
   files = os.listdir('.\sjis')
   for file in files:
     print(file)
-    with open('.\sjis\\' + file, encoding='shift_jis') as fi, open('.\\utf8\\' + file, 'w',encoding='utf-8') as fo:
+    with open('.\\sjis\\' + file, encoding='shift_jis') as fi, open('.\\utf8\\' + file, 'w',encoding='utf-8') as fo:
       fo.write(fi.read())
 
 if __name__ == '__main__':
